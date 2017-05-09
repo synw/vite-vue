3d42c7f5f2165418a1432d6791f0219fcc087c8f
Win <synwe@yahoo.com>
2017-05-09 13:39:22 +0000

    Corrections

--- src/vv.js
+++ src/vv.js
@@ -8,45 +8,45 @@ var vvMixin = {
     },
     methods: {
     	flush: function(preserve) {
-			if (vvDebug === true) {console.log("FLUSH"+"\n# active: "+this.active, this.active.length)};
+			if (vvDebug === true) {console.log("FLUSH"+"\n# active: "+store.getters.active, store.getters.active.length)};
 			var act = [];
-			for (i=0;i<this.active.length;i++) {
-				if (vvDebug === true) {if (preserve) {console.log("Preserve: "+this.active[i]+" / "+preserve)}};
-				if (this.active[i] != preserve) {
-					var t = typeOf(this.active[i]);
+			for (i=0;i<store.getters.active.length;i++) {
+				if (vvDebug === true) {if (preserve) {console.log("Preserve: "+store.getters.active[i]+" / "+preserve)}};
+				if (store.getters.active[i] != preserve) {
+					var t = typeOf(store.getters.active[i]);
 					if (isNaN(t)) {
-						if (vvDebug === true) { console.log("NaN value "+this.active[i])};
+						if (vvDebug === true) { console.log("NaN value "+store.getters.active[i])};
 						continue
 					}
 					if (t === "string") {
-						if (vvDebug === true) { console.log(+ " [x] Flushing "+this.active[i]+" (string)")};
-						this.active[i] = "";
+						if (vvDebug === true) { console.log(+ " [x] Flushing "+store.getters.active[i]+" (string)")};
+						store.getters.active[i] = "";
 					} else if (t === "array") {
-						if (vvDebug === true) { console.log(+ " [x] Flushing "+this.active[i]+" (array)")};
-						this.active[i] = [];
+						if (vvDebug === true) { console.log(+ " [x] Flushing "+store.getters.active[i]+" (array)")};
+						store.getters.active[i] = [];
 					} else if (t === "object") {
-						if (vvDebug === true) { console.log(+ " [x] Flushing "+this.active[i]+" (object)")};
-						this.active[i] = {}
+						if (vvDebug === true) { console.log(+ " [x] Flushing "+store.getters.active[i]+" (object)")};
+						store.getters.active[i] = {}
 					} else if (t === "boolean") {
-						if (vvDebug === true) { console.log(+ " [x] Flushing "+this.active[i]+" (boolean)")};
-						this.active[i] = false
+						if (vvDebug === true) { console.log(+ " [x] Flushing "+store.getters.active[i]+" (boolean)")};
+						store.getters.active[i] = false
 					} else if (t === "number") {
-						if (vvDebug === true) { console.log(+ " [x] Flushing "+this.active[i]+" (number)")};
-						this.active[i] = 0
+						if (vvDebug === true) { console.log(+ " [x] Flushing "+store.getters.active[i]+" (number)")};
+						store.getters.active[i] = 0
 					} else {
-						if (vvDebug === true) { console.log("Type not found "+this.active[i])};
+						if (vvDebug === true) { console.log("Type not found "+store.getters.active[i])};
 						continue
 					}
-					act.push(this.active[i]);
+					act.push(store.getters.active[i]);
 				} else {
-					if (vvDebug === true) { console.log("Preserving "+this.active[i])};
+					if (vvDebug === true) { console.log("Preserving "+store.getters.active[i])};
 				}
 			}
-			this.active = act;
-			if (vvDebug === true) { console.log("--> active: "+this.active+"\n ****** flushed *****\n") };
+			store.getters.active = act;
+			if (vvDebug === true) { console.log("--> active: "+store.getters.active+"\n ****** flushed *****\n") };
 		},
 		isActive: function(item) {
-			if (this.active.indexOf(item) > -1) {
+			if (store.getters.active.indexOf(item) > -1) {
 				return true
 			}
 			return false
